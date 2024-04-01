a8c9eaf122bca13d42f9c2b9bc9654a822dbee84
ivan-andriichak <picea.abies10@gmail.com>
2024-04-01 10:22:48 +0300

    Main branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { UsersComponent } from "./components/users/users.component";
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root', // Селектор елементу, за яким буде вибиратися компонент
   standalone: true, // Позначає, що компонент є автономним і не залежить від інших модулів
-  imports: [RouterOutlet, UsersComponent], // Імпорт модулів, але це некоректно, imports призначений для модулів, а не компонентів
+  imports: [RouterOutlet], // Імпорт модулів, але це некоректно, imports призначений для модулів, а не компонентів
   templateUrl: './app.component.html', // Шлях до файлу шаблону компонента
   styleUrl: './app.component.css' // Шлях до файлу стилів компонента
 })
